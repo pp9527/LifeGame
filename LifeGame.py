2450baa683527a75835c603356038e4ddddbe305
panwuzhen <2382852105@qq.com>
2020-04-01 15:08:54 +0800

    add sthing

--- LifeGame.py
+++ LifeGame.py
@@ -7,7 +7,7 @@ import sys
 
 
 def init_map(rows, cols):
-    #初始化指定长宽的地图, rows(int), cols(int), 返回第一张随机细胞状态图
+    # 初始化指定长宽的地图, rows(int), cols(int), 返回第一张随机细胞状态图
     initial_map = [[0 for i in range(cols)]for i in range(rows)]
     for i in range(0, rows):
         for j in range(0, cols):
@@ -16,49 +16,50 @@ def init_map(rows, cols):
 
 
 def cell_laws(mov_map):
-    #入口参数为一个二维列表，根据生命法则推演出下一张地图并返回
+    # 入口参数为一个二维列表，根据生命法则推演出下一张地图并返回
     live_cell_num = 0
     new_map = [[0 for i in range(len(mov_map[0]))]for i in range(len(mov_map))]
     for i in range(0, len(mov_map)):
         for j in range(0, len(mov_map[0])):
             if mov_map[i][j - 1] == 1:
-                #左
+                # 左
                 live_cell_num += 1
             if mov_map[i][(j + 1) % len(mov_map[0])] == 1:
-                #右
+                # 右
                 live_cell_num += 1
             if mov_map[i - 1][j] == 1:
-                #上
+                # 上
                 live_cell_num += 1
             if mov_map[(i + 1) % len(mov_map)][j] == 1:
-                #下
+                # 下
                 live_cell_num += 1
             if mov_map[i - 1][j - 1] == 1:
-                #上左
+                # 上左
                 live_cell_num += 1
             if mov_map[i - 1][(j + 1) % len(mov_map[0])] == 1:
-                #上右
+                # 上右
                 live_cell_num += 1
             if mov_map[(i + 1) % len(mov_map)][j - 1] == 1:
-                #下左
+                # 下左
                 live_cell_num += 1
             if mov_map[(i + 1) % len(mov_map)][(j + 1) % len(mov_map[0])] == 1:
-                #下右
+                # 下右
                 live_cell_num += 1
             if live_cell_num == 3:
-                #周围有三个活细胞，下一状态为活
+                # 周围有三个活细胞，下一状态为活
                 new_map[i][j] = 1
             elif live_cell_num == 2:
-                #周围有两个活细胞，保持原状态
+                # 周围有两个活细胞，保持原状态
                 new_map[i][j] = mov_map[i][j]
             else:
-                #其他情况下一状态为死
+                # 其他情况下一状态为死
                 new_map[i][j] = 0
             live_cell_num = 0
     return new_map
 
+
 def check_int(date):
-    #输入的地图长宽的合法性检测, int
+    # 输入的地图长宽的合法性检测, int
     try:
         int(date)
     except ValueError:
@@ -73,7 +74,7 @@ def check_int(date):
 
 
 def out_map(every_map):
-	#入口参数：arr[list][list], 返回演变出的下一张细胞状态图
+    # 入口参数：arr[list][list], 返回演变出的下一张细胞状态图
     pygame.init()
     pygame.display.set_caption('LifeGame')
     screen = pygame.display.set_mode([len(every_map[0]) * 6, len(every_map) * 6])
@@ -85,17 +86,17 @@ def out_map(every_map):
             else:
                 pygame.draw.rect(screen, [255, 255, 255], [j * 6, i * 6, 5, 5], 0)
     pygame.display.flip()
-    pygame.time.delay(15)
-    #程序延迟一段时间
+    pygame.time.delay(2)
+    # 程序延迟一段时间
     return 1
 
 
 map_rows = input("输入地图高度：")
 map_rows = check_int(map_rows)
-#行数 == 一维列表长度 == 地图高度
+# 行数 == 一维列表长度 == 地图高度
 map_cols = input("输入地图宽度：")
 map_cols = check_int(map_cols)
-#列数 == 二维数组长度 == 地图宽度
+# 列数 == 二维数组长度 == 地图宽度
 game_map = init_map(map_rows, map_cols)
 while 1:
     game_map = cell_laws(game_map)
