93cbd07ebaa13358b9875d39d12470b1e6d34b98
panwuzhen <2382852105@qq.com>
2020-04-01 14:46:11 +0800

    add init_map()

--- LifeGame.py
+++ LifeGame.py
@@ -8,6 +8,11 @@ import sys
 
 def init_map(rows, cols):
     #初始化指定长宽的地图, rows(int), cols(int), 返回第一张随机细胞状态图
+    initial_map = [[0 for i in range(cols)]for i in range(rows)]
+    for i in range(0, rows):
+        for j in range(0, cols):
+            initial_map[i][j] = random.randint(0, 1)
+    return initial_map
 
 
 def cell_laws(mov_map):
