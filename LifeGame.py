5026064921182b3b87ca5f682e781b8e5bf63fc8
panwuzhen <2382852105@qq.com>
2020-04-01 14:42:18 +0800

    create every functions

--- LifeGame.py
+++ LifeGame.py
@@ -1,2 +1,22 @@
 # -*- coding:utf-8 -*-
 # author by : 2382852105@qq.com and 2817393972@qq.com
+
+import random
+import pygame
+import sys
+
+
+def init_map(rows, cols):
+    #初始化指定长宽的地图, rows(int), cols(int), 返回第一张随机细胞状态图
+
+
+def cell_laws(mov_map):
+    #入口参数为一个二维列表，根据生命法则推演出下一张地图并返回
+
+
+def check_int(date):
+    #输入的地图长宽的合法性检测, int
+
+
+def out_map(every_map):
+	#入口参数：arr[list][list], 返回演变出的下一张细胞状态图
