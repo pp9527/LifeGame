3cea8c09485129826ae48403a61bd58763273f0c
panwuzhen <2382852105@qq.com>
2020-04-01 14:51:24 +0800

    add functions

--- LifeGame.py
+++ LifeGame.py
@@ -15,17 +15,17 @@ def init_map(rows, cols):
     return initial_map
 
 
-def print_map(every_map):
-    """打印地图"""
-    for i in range(0, len(every_map)):
-        for j in range(0, len(every_map[0])):
-            if every_map[i][j] == 0:
-                """0表示死细胞 打印□"""
-                print('□', end="")
-                """1表示活细胞 打印■"""
-            else:
-                print('■', end="")
-        print("")
+# def print_map(every_map):
+#     """打印地图"""
+#     for i in range(0, len(every_map)):
+#         for j in range(0, len(every_map[0])):
+#             if every_map[i][j] == 0:
+#                 """0表示死细胞 打印□"""
+#                 print('□', end="")
+#                 """1表示活细胞 打印■"""
+#             else:
+#                 print('■', end="")
+#         print("")
 
 
 def cell_laws(mov_map):
@@ -72,7 +72,44 @@ def cell_laws(mov_map):
 
 def check_int(date):
     #输入的地图长宽的合法性检测, int
+    try:
+        int(date)
+    except ValueError:
+        print("你输入的不是数字！")
+        sys.exit(0)
+    date = int(date)
+    if date < 1:
+        print("请输入大于一的数：")
+        sys.exit(0)
+    else:
+        return date
 
 
-# def out_map(every_map):
-# 	#入口参数：arr[list][list], 返回演变出的下一张细胞状态图
+def out_map(every_map):
+	#入口参数：arr[list][list], 返回演变出的下一张细胞状态图
+    pygame.init()
+    pygame.display.set_caption('LifeGame')
+    screen = pygame.display.set_mode([len(every_map[0]) * 6, len(every_map) * 6])
+    screen.fill([255, 255, 255])
+    for i in range(0, len(every_map)):
+        for j in range(0, len(every_map[0])):
+            if every_map[i][j] == 1:
+                pygame.draw.rect(screen, [255, 0, 0], [j * 6, 6 * i, 5, 5], 0)
+            else:
+                pygame.draw.rect(screen, [255, 255, 255], [j * 6, i * 6, 5, 5], 0)
+    pygame.display.flip()
+    pygame.time.delay(15)
+    """程序延迟一段时间"""
+    return 1
+
+
+map_rows = input("输入地图高度：")
+map_rows = check_int(map_rows)
+"""行数 == 一维列表长度 == 地图高度"""
+map_cols = input("输入地图宽度：")
+map_cols = check_int(map_cols)
+"""列数 == 二维数组长度 == 地图宽度"""
+game_map = init_map(map_rows, map_cols)
+while 1:
+    game_map = cell_laws(game_map)
+    out_map(game_map)
